ab97409e1de1880fa44adb5f92c88be0bf57ad3a
sba22230 <sba22230@student.cct.ie>
2023-03-27 21:31:04 +0100

    updated tutorial

--- tutorial_5_pt1.sh
+++ tutorial_5_pt1.sh
@@ -7,5 +7,18 @@ mysql -uroot -p
 
 mysql>create database Test;
 mysql>use Test;
-mysql>CREATE TABLE testtable (id INTEGER PRIMARY KEY,
-description VARCHAR(255));
+mysql>CREATE TABLE testtable (id INTEGER PRIMARY KEY, description VARCHAR(255));
+
+read -p "Exit the MySql session" </dev/tty
+
+mysql> sudo mysql
+mysql> UPDATE mysql.user SET authentication_string=null WHERE User='root';
+mysqL> flush privileges;
+mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+mysql> flush privileges;
+Exit MySQL.
+mysql> exit
+ 
+mysql> mysql -uroot -p
+
