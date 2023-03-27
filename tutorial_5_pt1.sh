9e282f5af3766a695307f90aae7f05250d9f5710
sba22230 <sba22230@student.cct.ie>
2023-03-27 21:51:56 +0100

    adding tutorial 5 files

--- tutorial_5_pt1.sh
+++ tutorial_5_pt1.sh
@@ -5,20 +5,20 @@ sudo apt install libmariadb-java
 
 mysql -uroot -p
 
-mysql>create database Test;
-mysql>use Test;
-mysql>CREATE TABLE testtable (id INTEGER PRIMARY KEY, description VARCHAR(255));
+# mysql>create database Test;
+# mysql>use Test;
+# mysql>CREATE TABLE testtable (id INTEGER PRIMARY KEY, description VARCHAR(255));
 
 read -p "Exit the MySql session" </dev/tty
 
-mysql> sudo mysql
-mysql> UPDATE mysql.user SET authentication_string=null WHERE User='root';
-mysqL> flush privileges;
-mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+# mysql> sudo mysql
+# mysql> UPDATE mysql.user SET authentication_string=null WHERE User='root';
+# mysqL> flush privileges;
+# mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
-mysql> flush privileges;
-Exit MySQL.
-mysql> exit
+# mysql> flush privileges;
+# Exit MySQL.
+# mysql> exit
  
-mysql> mysql -uroot -p
+# mysql> mysql -uroot -p
 
---
+++ tutorial_5_pt2.sh
@@ -0,0 +1,38 @@
+cd 
+cd Downloads
+wget https://downloads.apache.org/spark/spark-3.2.3/spark-3.2.3-bin-hadoop3.2.tgz 
+sudo tar -xvf spark-3.2.3-bin-hadoop3.2.tgz
+
+sudo mv ./spark-3.2.3-bin-hadoop3.2 /usr/local
+cd /usr/local
+sudo ln -sf ./spark-3.2.3-bin-hadoop3.2 spark
+
+sudo chown -R sba22230:hadoopgroup spark*
+
+cd /usr/local/spark
+./bin/pyspark
+read -p "Exit the pyspark session" </dev/tty
+
+ cd /usr/local
+
+sudo wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+
+sudo bash Anaconda3-2022.10-Linux-x86_64.sh
+
+cd /home/sba22230
+echo 'export SPARK_HOME=/usr/local/spark' >> ./.bashrc
+echo 'export PATH=$PATH:$SPARK_HOME/bin' >> ./.bashrc 
+echo 'export PYSPARK_PYTHON=/usr/local/anaconda3/bin/python3' >> ./.bashrc
+echo 'export PYSPARK_DRIVER_PYTHON=jupyter' >> ./.bashrc
+echo 'export PYSPARK_PYTHON=python3' >> ./.bashrc
+echo 'export PYSPARK_DRIVER_PYTHON_OPTS="notebook"' >> ./.bashrc
+
+source ./.bashrc
+
+pyspark
+
+sudo apt install jupyter
+
+pyspark
+
+read -p "Exit the pyspark session" </dev/tty
