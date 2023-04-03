f5773fa0f99f623b3f09ed5a578bcf3fcd8f6bfe
sba22230 <sba22230@student.cct.ie>
2023-04-03 21:48:13 +0100

    updated tutorial 5 files

--- tutorial_5_pt3.sh
+++ tutorial_5_pt3.sh
@@ -10,11 +10,12 @@ read -p "Check that Hadoop has started" </dev/tty
 # Go to the downloads folder
 cd ~
 cd Downloads
-unzip Tutorial_5_Spark_Word_Count_Example.zip
-cd Tutorial_5_Spark_Word_Count_Example
-cp ./*.* ../../Desktop/Lab05/
+mkdir Tutorial_5_Spark_WordCount_Example
+unzip Tutorial_5_Spark_WordCount_Example.zip -d Tutorial_5_Spark_WordCount_Example
+cd Tutorial_5_Spark_WordCount_Example
+cp ./*.* ../../Lab05/
 cd ~
-cd /Desktop/Lab05/
+cd Lab05/
 hadoop fs -put ./pg30123.txt /user1
 hadoop fs -ls /user1
 pyspark 
