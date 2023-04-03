5bd9ce186fcc9001f31a2af5c065186284be5aa7
sba22230 <sba22230@student.cct.ie>
2023-04-03 20:17:31 +0100

    Adding the last part of tutorial 5

--- tutorial_5_pt3.sh
+++ tutorial_5_pt3.sh
@@ -2,3 +2,22 @@ cd ~
 cd /Desktop
 mkdir Lab05
 cd Lab05
+
+start-dfs.sh
+start-yarn.sh
+jps
+read -p "Check that Hadoop has started" </dev/tty
+# Go to the downloads folder
+cd ~
+cd Downloads
+unzip Tutorial_5_Spark_Word_Count_Example.zip
+cd Tutorial_5_Spark_Word_Count_Example
+cp ./*.* ../../Desktop/Lab05/
+cd ~
+cd /Desktop/Lab05/
+hadoop fs -put ./pg30123.txt /user1
+hadoop fs -ls /user1
+pyspark 
+
+# in the jupyter notebook follow the steps in the tutorial
+
