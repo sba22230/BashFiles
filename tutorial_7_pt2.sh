578fa6eed7ac95d98c8c68c491fe6c1d71cbf34d
sba22230 <sba22230@student.cct.ie>
2023-04-10 16:16:43 +0100

    Adding final changes to Tutorial 7

--- tutorial_7_pt2.sh
+++ tutorial_7_pt2.sh
@@ -2,7 +2,7 @@ cd /usr/local/cassandra
 bin/nodetool status
 bin/nodetool ring
 
-
+sudo apt install python2
 read -p "check cassandra status and then launch cqlsh" </dev/tty
 
 bin/cqlsh
@@ -21,11 +21,11 @@ bin/cqlsh
 #CREATE TABLE IF NOT EXISTS home (home_id text PRIMARY KEY, address text, city text, state text, zip text, contact_name text, phone text, alt_phone text, phone_password text, email text, main_code text, guest_code text);
 #DESCRIBE Table home
 #CREATE TABLE IF NOT EXISTS activity (home_id text, datetime timestamp, code_used text, event text, PRIMARY KEY (datetime));
-#INSERT INTO activity (home_id, datetime, code_used, event) VALUES (1, '2021-10-01 12:00:00', 'main', 'entry');
+#INSERT INTO activity (home_id, datetime, code_used, event) VALUES (HO1, '2021-10-01 12:00:00', 'main', 'entry');
 #SELECT * FROM activity;
 
-#copy home_security.activity (home_id, datetime, code_used, event) FROM '/home/sba22230/Downloads/activity.csv' WITH HEADER = TRUE and delimiter = '|';
-#copy home_security.home (home_id, address, city, state, zip, contact_name, phone, alt_phone, phone_password, email, main_code, guest_code) FROM '/home/sba22230/Downloads/home.csv' WITH HEADER = TRUE and delimiter = '|';
+#copy home_security.activity (home_id, datetime, code_used, event) FROM '/home/sba22230Lab07/events.csv' WITH HEADER = TRUE and delimiter = '|';
+#copy home_security.home (home_id, address, city, state, zip, contact_name, phone, alt_phone, phone_password, email, main_code, guest_code) FROM '/home/sba22230/Lab07/homes.csv' WITH HEADER = TRUE and delimiter = '|';
 #SELECT * FROM home_security.home;
 #COPY home_security.activity TO '/home/sba22230/Downloads/activity_fromCassandra.csv' WITH HEADER = TRUE and delimiter = '|';
 
@@ -37,5 +37,5 @@ read -p "check employee_data.csv download status" </dev/tty
 #CREATE KEYSPACE IF NOT EXISTS employees with replication = {'class':'SimpleStrategy', 'replication_factor':1};
 #CREATE TABLE employees.employees_data (employee_id int PRIMARY KEY, firstname text, lastname text, department text, city text);
 #USE employees;
-#COPY employees.employees_data (employee_id, firstname, lastname, department, city) FROM '/home/sba22230/Downloads/employees_data.csv' WITH HEADER = true;
+#COPY employees.employees_data (employee_id, firstname, lastname, department, city) FROM '/home/sba22230/Lab07/employees_data.csv' WITH HEADER = true;
 #select * from employees.employees_data;
